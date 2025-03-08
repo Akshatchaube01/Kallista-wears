ab86b7eda18fc4a14ee7abd47f0e79859ccc098d
AkshatChaube01 <AkshatChaube11@gmail.com>
2025-03-08 15:18:06 +0530

    client commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import { Instagram, Facebook, Twitter } from "lucide-react";
 import { motion } from "framer-motion";
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: 'standalone', // Ensure Vercel builds correctly
-};
-
-export default nextConfig;
